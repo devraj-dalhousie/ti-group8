508ff07da79863908e33f06838942f1cfe263ec8
jaswanth106 <jaswanth106@localhost>
2022-04-04 02:44:54 -0300

    Made changes in project structure and integrated all the features in a smoother way.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
-import "./index.css";
-import App from "./App";
-import * as serviceWorker from "./serviceWorker";
+/**
+ * @author Mugdha Agharkar
+ */
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Routes from './Routes';
+import { BrowserRouter } from 'react-router-dom';
+import './index.css'
 
 ReactDOM.render(
+  <React.StrictMode>
     <BrowserRouter>
-        <App />
-    </BrowserRouter>,
-    document.getElementById("root")
+      <Routes />
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
-
-serviceWorker.unregister();
