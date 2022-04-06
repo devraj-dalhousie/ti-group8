1b6a9973ce91b5903b9c1226b53f84e4bb03295c
dv247596@dal.ca <dv247596@dal.ca>
2022-04-06 17:14:28 +0530

    Created activity page

--- src/pages/activity/activity.js
+++ src/pages/activity/activity.js
@@ -8,7 +8,39 @@ class Activity extends Component {
             <div>
                 <UserNavbar />
                 <div className={"activity-form"}>
-                    Hello world....
+                    <h4>Based your consent, we have tracked the health data from your Apple Watch and stored the data in our database. </h4>
+                    <h4>Here your health activity metrics. Please review it.</h4>
+                    <hr/>
+                    <table className={"health-metrics"}>
+                        <tr>
+                            <th>Health metrics</th>
+                            <th>Value</th>
+                        </tr>
+                        <tr>
+                            <td>Your avg daily steps(last 10 days)</td>
+                            <td>7562</td>
+                        </tr>
+                        <tr>
+                            <td>Your avg standing time(last 10 days)</td>
+                            <td>3 hour 42 minutes</td>
+                        </tr>
+                        <tr>
+                            <td>Your blood oxygen levels(last 24 hours)</td>
+                            <td>88</td>
+                        </tr>
+                        <tr>
+                            <td>Avg sleeping time(last 4 days)</td>
+                            <td>7 hour 23 minutes</td>
+                        </tr>
+                        <tr>
+                            <td>Avg daily calory burn</td>
+                            <td>56 kcal</td>
+                        </tr>
+                        <tr>
+                            <td>Avg daily running time</td>
+                            <td>14 minutes</td>
+                        </tr>
+                    </table>
                 </div>
             </div>
         );
