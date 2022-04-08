7d0687e536bb9afa941ddd4994c4024d533ede72
jaswanth106 <jaswanth106@localhost>
2022-04-08 00:08:41 -0300

    home and profile page updated

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,28 +1,167 @@
-import React, {Component} from 'react';
+import React, {useEffect, useState} from 'react';
 import UserNavbar from '../../components/navBar/UserNavBar';
 import './home.css';
-import {Link} from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import axios from "axios";
+import {CanvasJSChart} from 'canvasjs-react-charts'
+import ReactApexChart from 'react-apexcharts';
 
-class Home extends Component {
-    render() {
-        return (
+
+function Home () {
+    const error = '';
+    let fitnessData = {};
+    let dietData = {};
+    let fitnessLabel = []
+    const baseURL = "http://localhost:8080"
+    var React = require('react');
+    let caloriePieData = {
+          
+        series: [25,25,25,25],
+        options: {
+          chart: {
+            width: 380,
+            type: 'pie',
+          },
+          labels: ['Team A', 'Team B', 'Team C', 'Team D'],
+          responsive: [{
+            breakpoint: 480,
+            options: {
+              chart: {
+                width: 200
+              },
+              legend: {
+                position: 'bottom'
+              }
+            }
+          }]
+        },
+      };
+    const [loadPie, setLoadPie]= useState(false);
+
+    useEffect(() => {
+        validateUser();
+        getFitnessData();
+        getDietData();
+        },[]);
+    
+    const getFitnessData = () =>{
+        axios.get(baseURL + "/getFitnessData", { params: { age: 42 } })
+        .then(result => {
+            fitnessData = result.data;
+            setFitnessData();
+            caloriePieData.options.labels = fitnessLabel;
+            setLoadPie(true);
+          }
+          ).then(err=> console.log(err))
+      
+    }
+
+    const getDietData = () =>{
+        axios.get(baseURL + "/getDietData", { params: { age: 42 } })
+        .then(result => {
+            dietData = result.data;
+            setDietData();
+          }
+          ).then(err=> console.log(err))
+      
+    }
+
+
+    const setFitnessData = () => {
+        const day = document.getElementById('fitness-day').value;
+        document.getElementById('restDays').innerHTML = "";
+        let fitenessTableHTML = '';
+            for(const data of fitnessData[day]){
+                fitenessTableHTML = fitenessTableHTML + '<tr><td>'+data[0]+'</td><td>'+data[1]+
+                '</td><td>'+data[2]+'</td><td>'+data[3]+'</td></tr>';
+                fitnessLabel.push(String(data[0]));
+            }
+        document.getElementById('fitnessData').innerHTML = fitenessTableHTML;
+    }
+
+    const setDietData = () =>{
+        const day = document.getElementById('fitness-day').value;
+        let dietTableHTML = '';
+        dietTableHTML = dietTableHTML + '<tr><td>'+dietData[day][0]+'</td><td>'+dietData[day][1]+
+                '</td><td>'+dietData[day][2]+'</td><td>'+dietData[day][3]+'</td><td>'+dietData[day][4]+'</td></tr>';
+        document.getElementById('dietData').innerHTML = dietTableHTML;
+    }
+
+    const validateUser = () =>  {
+        console.log('inside validate');
+        const isBasicValidationPassed = doBasicValidation();
+        if (isBasicValidationPassed === false) {
+            window.location.href = window.location.origin + "/profile?user=" + localStorage.getItem('email');
+            return;
+        }
+    }
+    
+    const doBasicValidation = () =>  {
+        const user =localStorage.getItem('email');
+        const userData = localStorage.getItem(user+'-data')
+        console.log(user)
+        if(userData === null){
+            return false;
+        }
+        return true;
+    }
+
+    const setAllData = () =>{
+        getFitnessData();
+        getDietData();
+    }
+
+    const checkNotEmpty = (value) => {
+        return value.trim() !== "";
+    }
+
+    return(
             <div>
             <UserNavbar />
-            <div className={"home-page"}>
-                Hello, welcome to Admix Fitness –(Blending Workout and Dietary Regimes) home page.
-                <br/>
-                We strive hard to keep you fit & healthy through a range of holistic offerings that include fitness and dietary program.
-                Now anyone can now stay healthy from the safety of their homes with our curated solution designed.
-                <br/>
-                We offer various paid programs which will keep to healthy and fit. See our plans here:
-                <br/>
-                <Link to={"/subscribe"}>See our plans</Link>
-                <br/>
-                #BeFitBeHealthy
-                </div>
+            <div className={"home-page"} id='homePage' name={localStorage.getItem('email')}>
+            <div style={{paddingTop:'120px'}}>
+               <h2 style={{textAlign:'center'}}>Your Fitness Plan:</h2><br />
+               <select name="day" id="fitness-day" onChange={setAllData}>
+                    <option value="Monday" >Monday</option>
+                    <option value="Tuesday">Tuesday</option>
+                    <option value="Wednesday">Wednesday</option>
+                    <option value="Thursday">Thursday</option>
+                    <option value="Friday">Friday</option>
+                    <option value="Saturday">Saturday</option>
+                    <option value="Sunday">Sudnday</option>
+                </select>
+                <table class="table">
+                    <thead class="thead-dark">
+                        <tr>
+                        <th style={{fontSize:'15pt'}}>Exercise</th>
+                        <th style={{fontSize:'15pt'}}>Repititions</th>
+                        <th style={{fontSize:'15pt'}}>Sets</th>
+                        <th style={{fontSize:'15pt'}}>Gap between Sets (min)</th>
+                        </tr>
+                    </thead>
+                    <tbody id='fitnessData'>
+                    </tbody>
+                </table>
+                <h2 style={{textAlign:'center'}}><p id='restDays'></p></h2>
+
+                <h2 style={{textAlign:'center'}}>Your Diet Plan:</h2>
+                <table class="table">
+                    <thead class="thead-dark">
+                        <tr>
+                        <th style={{fontSize:'15pt'}}>Breakfast</th>
+                        <th style={{fontSize:'15pt'}}>Lunch</th>
+                        <th style={{fontSize:'15pt'}}>Dinner</th>
+                        <th style={{fontSize:'15pt'}}>Snacks 1</th>
+                        <th style={{fontSize:'15pt'}}>Snacks 2</th>
+                        </tr>
+                    </thead>
+                    <tbody id='dietData'>
+                    </tbody>
+                </table>
+               </div>
+            </div>
             </div>
         );
-    }
+    
 }
-
 export default Home;
--- src/pages/profile/profile.js
+++ src/pages/profile/profile.js
@@ -1,29 +1,105 @@
 import React, {Component} from 'react';
+import { useState, useEffect } from 'react'
 import {Link} from "react-router-dom";
 import './profile.css';
 import UserNavbar from '../../components/navBar/UserNavBar';
-import HealthDetailsSuccess from "./healthDetailsSuccess";
 
 class Profile extends Component {
     constructor(props) {
         super(props);
-        this.state = {redirect: false};
+        this.error = '';
+        this.user= window.location.search !== "" ? window.location.search.substring(6) : localStorage.getItem('email');
+        this.doBasicValidation = this.doBasicValidation.bind(this);
         this.validateForm = this.validateForm.bind(this);
+        this.checkNotEmpty = this.checkNotEmpty.bind(this);
     }
 
-    async validateForm(event) {
+    validateForm(event) {
         event.preventDefault();
-        this.setState({redirect: true});
+        const isBasicValidationPassed = this.doBasicValidation();
+        if (isBasicValidationPassed === false) {
+            console.log("Basic validation failed.")
+            const errorsDiv = document.getElementById("errors");
+            errorsDiv.innerHTML = '<p style="color:red">' + this.error + '</p>'
+            return;
+        }
+            localStorage.setItem(this.user+'-data', "done")
+            window.location.href = window.location.origin + "/home?user=" + this.user;
+    }
+    
+    doBasicValidation() {
+        console.log("Doing basic validation");
+        var errors = '';
+        const age = document.getElementById("age");
+        if (this.checkNotEmpty(age.value) === false || isNaN(String(age.value).trim())) {
+            errors = errors + "Invalid Age.<br>";
+        }
+
+        const weight = document.getElementById("weight");
+        if (this.checkNotEmpty(weight.value) === false || isNaN(String(weight.value).trim())) {
+            errors = errors + "Invalid weight.<br>";
+        }
+
+        const height = document.getElementById("height");
+        if (this.checkNotEmpty(height.value) === false || isNaN(String(height.value).trim())) {
+            errors = errors + "Invalid height.<br>";
+        }
+
+        const wearableYes = document.getElementById("wearable");
+        if (this.checkNotEmpty(wearableYes.value) === false) {
+            errors = errors + "Invalid wearable.<br>";
+        }
+
+        const devices = document.getElementById("devices");
+        if (this.checkNotEmpty(devices.value) === false) {
+            errors = errors + "Invalid devices.<br>";
+        }
+
+        const gender = document.getElementById("gender");
+        if (this.checkNotEmpty(gender.value) === false) {
+            errors = errors + "Invalid gender.<br>";
+        }
+
+        const drinking = document.getElementById("drinking");
+        if (this.checkNotEmpty(drinking.value) === false) {
+            errors = errors + "Invalid drinking habits.<br>";
+        }
+
+        const smoking = document.getElementById("smoking");
+        if (this.checkNotEmpty(smoking.value) === false) {
+            errors = errors + "Invalid smoking habits.<br>";
+        }
+
+        const healthGoal = document.getElementById("healthGoal");
+        if (this.checkNotEmpty(healthGoal.value) === false) {
+            errors = errors + "Invalid health goals.<br>";
+        }
+        this.error = errors;
+        
+        if(this.checkNotEmpty(errors)){
+            return false;
+        } else{
+            localStorage.setItem(this.user+'-age', age.value)
+            localStorage.setItem(this.user+'-weight', weight.value)
+            localStorage.setItem(this.user+'-height', height.value)
+            localStorage.setItem(this.user+'-wearable', wearableYes.value)
+            localStorage.setItem(this.user+'-devices', devices.value)
+            localStorage.setItem(this.user+'-gender', gender.value)
+            localStorage.setItem(this.user+'-drinking', drinking.value)
+            localStorage.setItem(this.user+'-smoking', smoking.value)
+            localStorage.setItem(this.user+'-healthGoal', healthGoal.value)
+            return true;
+        }
+
+        
+    }
+
+    checkNotEmpty(value) {
+        return value.trim() !== "";
+
     }
 
     render() {
-        if(this.state.redirect) {
-            return (
-                <div>
-                    <HealthDetailsSuccess />
-                </div>
-            )
-        }
         return (
             <div>
             <UserNavbar />
@@ -37,7 +113,7 @@ class Profile extends Component {
 
                     <div className="form-group">
                         <label>Weight(in KG)</label>
-                        <input type="text" id="lname" className="profile-form-group" placeholder="Enter weight in kg" />
+                        <input type="text" id="weight" className="profile-form-group" placeholder="Enter weight in kg" />
                     </div>
 
                     <div className="form-group">
@@ -48,67 +124,70 @@ class Profile extends Component {
                     <div className={"profile-form-group"}>
                         <div className={"doYouWear"}>
                             <label>Do you wear wearables?</label>
-                            <label htmlFor="yes">Yes</label>
-                            <input type="radio" name="doYouWear" id="yes" value="yes" />
-                            <label htmlFor="no">No</label>
-                            <input type="radio" name="doYouWear" id="no" value="no"/>
+                            <select name="devices" id="wearable">
+                            <option name="" value=""></option>
+                            <option value="yes">Yes</option>
+                            <option value="No">No</option>
+                            </select>
                         </div>
                         <small>(By answering this question you give your consent to track the data from the wearable device.)</small>
                     </div>
                     <hr/>
                     <label htmlFor="devices">Which wearable device do you wear:</label>
                     <select name="devices" id="devices">
+                        <option value=""></option>
                         <option value="apple">Apple Watch</option>
                         <option value="fitbit">Fitbit</option>
                         <option value="samsung">Samsung Watch</option>
                         <option value="xiaomi">Xiaomi Smart Band</option>
                         <option value="goqii">Goqii</option>
+                        <option value="other">Others</option>
                     </select>
                 <hr/>
                     <div className={"profile-form-group"}>
                         <div className={"gender"}>
                         <label>Gender</label>
-                        <label htmlFor="male">Male</label>
-                        <input type="radio" name="gender" id="male" value="male" />
-                        <label htmlFor="female">Female</label>
-                        <input type="radio" name="gender" id="female" value="female"/>
+                        <select name="gender" id="gender">
+                            <option value=''></option>
+                            <option name="male" value="male">Male</option>
+                            <option value="female">Female</option>
+                            <option value="others">Others</option>
+                            </select>
                         </div>
                     </div>
                     <hr/>
                     <div className="drink-input">
-                        <label>Do you drink?
-                        <label htmlFor="yes">Yes</label>
-                        <input type="radio" name="drink" id="yes" value="yes" />
-                        <label htmlFor="no">No</label>
-                        <input type="radio" name="drink" id="no" value="no"/>
-                        <label htmlFor="occ">Occasionally</label>
-                        <input type="radio" name="drink" id="occ" value="occ"/>
-                        </label>
+                        <label>Do you drink?</label>
+                        <select name="drinking" id="drinking">
+                            <option name="" value=""></option>
+                            <option value="yes">Yes</option>
+                            <option value="No">No</option>
+                            <option value="occ">Occationally</option>
+                        </select>
                     </div>
                     <hr/>
                     <div className="smoke-input">
                         <label>Do you smoke?</label>
-                        <label htmlFor="yes">Yes</label>
-                        <input type="radio" name="smoke" id="yes" value="yes" />
-                        <label htmlFor="no">No</label>
-                        <input type="radio" name="smoke" id="no" value="no"/>
-                        <label htmlFor="occ">Occasionally</label>
-                        <input type="radio" name="smoke" id="occ" value="occ"/>
+                        <select name="smoking" id="smoking">
+                            <option name="" value=""></option>
+                            <option value="yes">Yes</option>
+                            <option value="No">No</option>
+                            <option value="occ">Occationally</option>
+                        </select>
                     </div>
                     <hr/>
                     <div className="drink-input">
-                        <label>Please select health goals?
-                            <label htmlFor="yes">Weight loss with vegetarian diet</label>
-                            <input type="radio" name="drink" id="yes" value="yes" />
-                            <label htmlFor="yes">Weight loss with non-vegetarian diet</label>
-                            <input type="radio" name="drink" id="yes" value="yes" />
-                            <label htmlFor="yes">Weight gain with vegetarian diet</label>
-                            <input type="radio" name="drink" id="yes" value="yes" />
-                            <label htmlFor="yes">Weight gain with non-vegetarian diet</label>
-                            <input type="radio" name="drink" id="yes" value="yes" />
-                        </label>
+                        <label>Please select health goals?</label>
+                        <select name="healthGoal" id="healthGoal">
+                            <option name="" value=""></option>
+                            <option>Weight loss with vegetarian diet</option>
+                            <option>Weight loss with non-vegetarian diet</option>
+                            <option>Weight gain with vegetarian diet</option>
+                            <option>Weight gain with non-vegetarian diet</option>
+                        </select>
                     </div>
                     <hr/>
+                    <div id='errors'></div>
                     <button type="submit" className="btn btn-dark btn-lg btn-block">Submit</button>
                 </form>
             </div>
